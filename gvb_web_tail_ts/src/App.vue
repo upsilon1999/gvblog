e5b6cf6b271e6c70ae013b3e1cced4794caaad34
upsilon1999 <Upsilon1999@outlook.com>
2024-07-16 10:45:34 +0800

    tail项目初始化

---
+++ gvb_web_tail_ts/src/App.vue
@@ -0,0 +1,6 @@
+<template>
+  <h1 class="text-3xl text-blue-600 font-bold underline">Hello world!</h1>
+  <RouterView />
+</template>
+<script setup lang="ts"></script>
+<style scoped></style>
---
+++ gvb_web_tail_ts/src/views/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>首页--本项目采用vue3+ts+tailwindcss</div>
+</template>
+<script setup lang="ts"></script>
+<style scoped></style>
